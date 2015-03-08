a90724df238aed7eeba0946d5639b0237beb117f
cristian <cristian@192.168.5.104>
2015-03-08 01:10:39 -0300

    Commit

--- Module.php
+++ Module.php
@@ -4,4 +4,4 @@
  * It allows usage of this module even without composer.
  * The original Module.php is in 'src/SamUser' in order to respect PSR-0
  */
-require_once __DIR__ . '/src/CdiCalendar/Module.php';
+require_once __DIR__ . '/src/CdiCommons/Module.php';
