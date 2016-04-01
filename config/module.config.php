9a6df024eb3b67ce40b07d2024601f11d4a7929a
cristian <cristian@192.168.1.112>
2016-03-31 22:43:31 -0300

    Commit

--- config/module.config.php
+++ config/module.config.php
@@ -4,18 +4,31 @@ return array(
     'cdicommons_options' => array(
     ),
     'controller_plugins' => array(
-		'aliases' => array(
-			'CsvExport' => 'CdiCsv:CsvExport',
-			'CsvImport' => 'CdiCsv:CsvImport',
-		),
-		'invokables' => array(
-			'CdiCsv:CsvExport' => 'CdiCommons\Controller\Plugin\CsvExport',
-			'CdiCsv:CsvImport' => 'CdiCommons\Controller\Plugin\CsvImport',
-		),
-		'shared' => array(
-			'CdiCsv:CsvExport' => false,
-			'CdiCsv:CsvImport' => false,
-		),
-	),
+        'aliases' => array(
+            'CsvExport' => 'CdiCsv:CsvExport',
+            'CsvImport' => 'CdiCsv:CsvImport',
+        ),
+        'invokables' => array(
+            'CdiCsv:CsvExport' => 'CdiCommons\Controller\Plugin\CsvExport',
+            'CdiCsv:CsvImport' => 'CdiCommons\Controller\Plugin\CsvImport',
+        ),
+        'shared' => array(
+            'CdiCsv:CsvExport' => false,
+            'CdiCsv:CsvImport' => false,
+        ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            'cdicommons_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'paths' => __DIR__ . '/../src/CdiCommons/Entity',
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'CdiCommons\Entity' => 'cdicommons_entity',
+                ),
+            ),
+        ),
+    ),
 );
- 
+
