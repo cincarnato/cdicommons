0a00e77e1f6be8d8848fbd999149f4eec21eb234
cristian <cristian@CINCARNATO>
2016-08-06 19:09:45 -0300

    Add stringarea

--- config/module.config.php
+++ config/module.config.php
@@ -4,23 +4,28 @@ return array(
     'cdicommons_options' => array(
     ),
     'controller_plugins' => array(
-		'aliases' => array(
-			'CsvExport' => 'CdiCsv:CsvExport',
-			'CsvImport' => 'CdiCsv:CsvImport',
-		),
-		'invokables' => array(
-			'CdiCsv:CsvExport' => 'CdiCommons\Controller\Plugin\CsvExport',
-			'CdiCsv:CsvImport' => 'CdiCommons\Controller\Plugin\CsvImport',
-		),
-		'shared' => array(
-			'CdiCsv:CsvExport' => false,
-			'CdiCsv:CsvImport' => false,
-		),
-	),
-      'view_manager' => array(
+        'aliases' => array(
+            'CsvExport' => 'CdiCsv:CsvExport',
+            'CsvImport' => 'CdiCsv:CsvImport',
+        ),
+        'invokables' => array(
+            'CdiCsv:CsvExport' => 'CdiCommons\Controller\Plugin\CsvExport',
+            'CdiCsv:CsvImport' => 'CdiCommons\Controller\Plugin\CsvImport',
+        ),
+        'shared' => array(
+            'CdiCsv:CsvExport' => false,
+            'CdiCsv:CsvImport' => false,
+        ),
+    ),
+    'view_helpers' => array(
+        'invokables' => array(
+            'CdiRenderForm' => 'CdiCommons\View\Helper\CdiRenderForm',
+        )
+    ),
+    'view_manager' => array(
         'template_path_stack' => array(
             'cdicommons' => __DIR__ . '/../view',
         ),
     ),
 );
- 
+
---
+++ src/CdiCommons/View/Helper/CdiRenderForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CdiCommons\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ *
+ * @author cincarnato
+ */
+class CdiFormElement extends AbstractHelper {
+
+    public function __invoke($templateForm, $templateElement) {
+
+        $partial = 'cdicommons/forms/' . $templateForm;
+        $partialElement = 'cdicommons/forms/elements/' . $templateElement;
+
+        return $this->view->partial($partial, array(
+                    "partialElement" => $partialElement));
+    }
+
+}
+
+?>
