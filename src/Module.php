4beab17aab54cf4308b033f5b58fcb31bb08faf9
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2018-11-16 17:06:34 -0300

    Fix Path

--- src/Module.php
+++ src/Module.php
@@ -5,10 +5,10 @@ namespace CdiCommons;
 class Module {
 
     public function getConfig() {
-        return include __DIR__ . '/../../config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function getServiceConfig() {
-            return include __DIR__ . '/../../config/services.config.php';
+            return include __DIR__ . '/../config/services.config.php';
     }
 }
